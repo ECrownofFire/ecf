209d8811eb85cef8868c07d3a2a2d7bf351f18bf
ECrownofFire <ECrownofFire@gmail.com>
2018-06-19 16:26:11 -0400

    Change user handling so it's a bit cleaner

--- src/ecf_user_handler.erl
+++ src/ecf_user_handler.erl
@@ -6,71 +6,63 @@
 
 init(Req = #{method := <<"GET">>}, State) ->
     User = ecf_utils:check_user_session(Req),
-    Req2 = handle_get(Req, User, cowboy_req:binding(action, Req)),
+    Id = cowboy_req:binding(id, Req, -1),
+    Action = cowboy_req:binding(action, Req),
+    Req2 = handle_get(Req, User, Id, Action),
     {ok, Req2, State};
 init(Req = #{method := <<"POST">>}, State) ->
     User = ecf_utils:check_user_session(Req),
-    Id = cowboy_req:binding(id, Req, -1),
-    Req2 = handle_post(Req, User, Id, cowboy_req:binding(action, Req)),
+    Action = cowboy_req:binding(action, Req),
+    Req2 = handle_post(Req, User, Action),
     {ok, Req2, State}.
 
-handle_get(Req, User, undefined) ->
-    case cowboy_req:binding(id, Req, -1) of
-        -1 ->
-            Html = ecf_generators:generate(user, User, User),
+handle_get(Req, User, -1, undefined) ->
+    Html = ecf_generators:generate(user, User, User),
+    cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
+                     Html, Req);
+handle_get(Req, User, Id, undefined) ->
+    case ecf_user:get_user(Id) of
+        undefined ->
+            ecf_utils:reply_status(404, User, false, Req);
+        Profile ->
+            Html = ecf_generators:generate(user, User, Profile),
             cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
-                             Html, Req);
-        Id ->
-            case ecf_user:get_user(Id) of
-                undefined ->
-                    ecf_utils:reply_status(404, User, false, Req);
-                Profile ->
-                    Html = ecf_generators:generate(user, User, Profile),
-                    cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
+                             Html, Req)
+    end;
+handle_get(Req, undefined, _, <<"edit">>) ->
+    ecf_utils:reply_status(401, undefined, edit_user_401, Req);
+handle_get(Req, User, _, <<"edit">>) ->
+    #{id := Id} = cowboy_req:match_qs([{id, int, ecf_user:id(User)}], Req),
+    case ecf_user:get_user(Id) of
+        undefined ->
+            ecf_utils:reply_status(404, User, false, Req);
+        Profile ->
+            case ecf_user:id(User) =:= Id
+                 orelse ecf_perms:check_perm_global(User, edit_user) of
+                false ->
+                    ecf_utils:reply(403, User, edit_user_403, Req);
+                true ->
+                    Html = ecf_generators:generate(user_edit, User, Profile),
+                    cowboy_req:reply(200,
+                                     #{<<"content-type">> => <<"text/html">>},
                                      Html, Req)
             end
     end;
-handle_get(Req, undefined, <<"edit">>) ->
-    ecf_utils:reply_status(401, undefined, edit_user_401, Req);
-handle_get(Req, User, <<"edit">>) ->
-    case cowboy_req:binding(id, Req, -1) of
-        -1 ->
-            Html = ecf_generators:generate(user_edit, User, User),
-            cowboy_req:reply(200, #{<<"content-type">> => <<"text/html">>},
-                             Html, Req);
-        Id ->
-            case ecf_user:get_user(Id) of
-                undefined ->
-                    ecf_utils:reply(404, User, false, Req);
-                Profile ->
-                    case ecf_perms:check_perm_global(User, edit_user) of
-                        false ->
-                            ecf_utils:reply(403, User, edit_user_403, Req);
-                        true ->
-                            Html = ecf_generators:generate(user_edit,
-                                                           User, Profile),
-                            cowboy_req:reply(200,
-                                             #{<<"content-type">>
-                                               => <<"text/html">>},
-                                             Html, Req)
-                    end
-            end
-    end;
-handle_get(Req, User, _) ->
+handle_get(Req, User, _, _) ->
     ecf_utils:reply_status(404, User, false, Req).
 
 
-handle_post(Req, User, _, undefined) ->
+handle_post(Req, User, undefined) ->
     ecf_utils:reply_status(405, User, user_405, Req);
-handle_post(Req, undefined, _, _) ->
+handle_post(Req, undefined, _) ->
     ecf_utils:reply_status(401, undefined, user_401, Req);
-handle_post(Req, User, -1, <<"edit">>) ->
-    ecf_utils:reply_status(400, User, edit_user_401, Req);
-handle_post(Req0, User, Id, <<"edit">>) ->
+handle_post(Req0, User, <<"edit">>) ->
+    {ok, KV, Req} = cowboy_req:read_urlencoded_body(Req0),
+    {_, Id0} = lists:keyfind(<<"id">>, 1, KV),
+    Id = binary_to_integer(Id0),
     case ecf_user:id(User) =:= Id
          orelse ecf_perms:check_perm_global(User, edit_user) of
         true ->
-            {ok, KV, Req} = cowboy_req:read_urlencoded_body(Req0),
             Bday = case lists:keyfind(<<"bday">>, 1, KV) of
                        {_, <<"">>} -> undefined;
                        {_, Bday0} -> iso8601:parse(Bday0);
